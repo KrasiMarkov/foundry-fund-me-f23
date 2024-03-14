ae57ef775c29c496dd52ffbf24e6586c84b7f363
KrasiMarkov <krasimarkov5@gmail.com>
2024-03-14 23:54:01 +0200

    added tests for fund function

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -9,11 +9,16 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 contract FundMeTest is Test {
     FundMe fundMe;
 
-    function setUp() external {
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether;
 
+    function setUp() external {
         DeployFundMe deployfundMe = new DeployFundMe();
 
         fundMe = deployfundMe.run();
+
+        vm.deal(USER, STARTING_BALANCE);
     }
 
     function testMinimumDollarIsFive() public {
@@ -31,5 +36,22 @@ contract FundMeTest is Test {
         uint256 version = fundMe.getVersion();
 
         assertEq(version, 4);
+    }
+
+    function testFundFailsWithoutEnoughETH() public {
+        vm.expectRevert(); // hey, the next line should revert
+
+        // assert(This tx fails/reverts)
+        fundMe.fund(); // send 0 value
+    }
+
+    function testFundUpdatesFundedDataStructure() public {
+        vm.prank(USER); // The next TX will be sent by USER
+
+        fundMe.fund{value: SEND_VALUE}();
+
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+
+        assertEq(amountFunded, SEND_VALUE);
     }
 }
